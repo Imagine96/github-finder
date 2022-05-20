244c562594c49c21b4c90e84a7c61c43636171d6
Ruben Guedes <imagine@macbook-air-de-ruben.home>
2022-05-20 17:21:02 +0200

    fetchers

--- src/utils/fetchRepos.ts
+++ src/utils/fetchRepos.ts
@@ -1,4 +1,9 @@
 import { GithubApiUserReposResp } from "./types/githubApiTypes"
+
+const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
 export const fetchRepos = async (target: string): Promise<GithubApiUserReposResp[]> => {
 
     const params = new URLSearchParams({
@@ -6,7 +11,7 @@ export const fetchRepos = async (target: string): Promise<GithubApiUserReposResp
         per_page: "10",
     })
 
-    const resp = await fetch(`${process.env.VITE_APP_GHURL}/users/${target}/repos?${params}`)
+    const resp = await fetch(`${process.env.VITE_APP_GHURL}/users/${target}/repos?${params}`, { headers })
     const data = (await resp.json()) as GithubApiUserReposResp[]
     return data
 }
--- src/utils/fetchUsers.ts
+++ src/utils/fetchUsers.ts
@@ -1,10 +1,18 @@
 import { GithubApiSearchUserResp, GithubApiRespUsers, GithubApiRespUserDetails } from "./types/githubApiTypes"
 import { fetchRepos } from "./fetchRepos"
+
+const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
 export const fetchUsers = async (target?: string): Promise<GithubApiRespUsers[]> => {
     if (target) {
         const resp = await fetch(`${process.env.VITE_APP_GHURL}/search/users?q=${target}`, {
             headers: {
-                Authorization: `token ${process.env.VITE_APP_GHT}`
+                Authorization: `token ${process.env.VITE_APP_GHT}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+
             }
         })
         const data = (await resp.json()) as GithubApiSearchUserResp
@@ -12,7 +20,9 @@ export const fetchUsers = async (target?: string): Promise<GithubApiRespUsers[]>
     } else {
         const resp = await fetch(`${process.env.VITE_APP_GHURL}/users`, {
             headers: {
-                Authorization: `token ${process.env.VITE_APP_GHT}`
+                Authorization: `token ${process.env.VITE_APP_GHT}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             }
         })
         const data = (await resp.json()) as GithubApiRespUsers[]
@@ -21,7 +31,13 @@ export const fetchUsers = async (target?: string): Promise<GithubApiRespUsers[]>
 }
 
 export const fetchUserDetails = async (username: string): Promise<{ userDetails: GithubApiRespUserDetails, repos: any[] }> => {
-    const resp = await fetch(`${process.env.VITE_APP_GHURL}/users/${username}`)
+    const resp = await fetch(`${process.env.VITE_APP_GHURL}/users/${username}`, {
+        headers: {
+            Authorization: `token ${process.env.VITE_APP_GHT}`,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
     const data = (await resp.json()) as GithubApiRespUserDetails
 
     const repos = await fetchRepos(username)
